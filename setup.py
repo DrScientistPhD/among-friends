d81d519ef249971bf585c97620fb20fee6faeada
Raymond Pasek <rcpasek@gmail.com>
2023-07-19 21:06:54 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A project dedicated to analytics, machine learning, and generative AI using group text chats.',
+    author='Raymond Pasek',
+    license='MIT',
+)
