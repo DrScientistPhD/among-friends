1593478f264dfc108549241bf66a2364dc262be2
Raymond Pasek <rcpasek@gmail.com>
2023-07-25 20:13:24 -0400

    update csv_read and start new script

--- src/data/csv_reader.py
+++ src/data/csv_reader.py
@@ -3,12 +3,12 @@ import pandas as pd
 
 class CSVReader:
     @staticmethod
-    def read_csv_as_dataframe(file_path: str) -> pd.DataFrame:
+    def read_csv_as_dataframe(file_name: str) -> pd.DataFrame:
         """
         Reads a CSV file as a pandas DataFrame.
 
         Args:
-            file_path (str): The path to the CSV file.
+            file_name (str): The name of the CSV file.
 
         Returns:
             pd.DataFrame: The pandas DataFrame containing the data from the CSV file.
@@ -18,12 +18,12 @@ class CSVReader:
             pd.errors.EmptyDataError: If the CSV file is empty or cannot be read.
         """
         # Using isinstance to validate the input argument.
-        if not isinstance(file_path, str):
+        if not isinstance(file_name, str):
             raise ValueError("file_path must be a string")
 
         try:
             # Reading the CSV file into a DataFrame.
-            dataframe = pd.read_csv(file_path)
+            dataframe = pd.read_csv("/Users/raymondpasek/Repos/among-friends/data/raw/message.csv" + file_name)
 
             return dataframe
         except pd.errors.EmptyDataError as e:
