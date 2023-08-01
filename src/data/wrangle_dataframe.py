12900b71aca42193ce3c0715acb3170f637ad000
Raymond Pasek <rcpasek@gmail.com>
2023-07-31 21:04:58 -0400

    adding time diff function and decay functions

--- src/data/wrangle_dataframe.py
+++ src/data/wrangle_dataframe.py
@@ -1,4 +1,7 @@
+import warnings
+
 import emoji
+import numpy as np
 import pandas as pd
 
 
@@ -79,9 +82,9 @@ class DataFrameWrangler:
         ]
 
         # Selecting the filtered columns and creating a copy
-        select_df = renamed_df[columns_to_filter].copy()
+        slim_df = renamed_df[columns_to_filter].copy()
 
-        return select_df
+        return slim_df
 
     @staticmethod
     def filter_and_rename_reactions_df(df: pd.DataFrame) -> pd.DataFrame:
@@ -120,14 +123,14 @@ class DataFrameWrangler:
         ]
 
         # Selecting the filtered columns and creating a copy
-        select_df = renamed_df[columns_to_filter].copy()
+        slim_df = renamed_df[columns_to_filter].copy()
 
         # Translating emojis in the "emoji" column and creating a new column "reaction_translation"
-        select_df["reaction_translation"] = select_df["emoji"].apply(
+        slim_df["reaction_translation"] = slim_df["emoji"].apply(
             DataFrameWrangler.translate_emoji
         )
 
-        return select_df
+        return slim_df
 
     @staticmethod
     def translate_emoji(text: str) -> str:
@@ -161,3 +164,176 @@ class DataFrameWrangler:
                 translated_text.append("<emoji_not_translated>")
 
         return " ".join(translated_text)
+
+    @staticmethod
+    def create_quotation_response_df(df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Creates a DataFrame containing both quotations and responses from a message DataFrame.
+
+        Args:
+            df (pd.DataFrame): The original DataFrame containing messages.
+
+        Returns:
+            pd.DataFrame: A new DataFrame containing both quotations and responses.
+
+        Raises:
+            TypeError: If dataframe is not a pandas DataFrame.
+        """
+        # Check if the input is a DataFrame
+        if not isinstance(df, pd.DataFrame):
+            raise TypeError("dataframe must be a pandas DataFrame")
+
+        # Renaming columns for quotations
+        quotation_df = df.rename(
+            columns={
+                "_id": "quotation_id",
+                "date_sent": "quotation_date_sent",
+                "thread_id": "quotation_thread_id",
+                "from_recipient_id": "quotation_from_recipient_id",
+                "body": "quotation_body",
+            }
+        )
+
+        # Renaming columns for responses
+        response_df = df.copy().rename(
+            columns={
+                "_id": "response_id",
+                "date_sent": "response_date_sent",
+                "thread_id": "response_thread_id",
+                "from_recipient_id": "response_from_recipient_id",
+                "body": "response_body",
+            }
+        )
+
+        # Convert quote_id to datetime64
+        response_df["quote_id"] = pd.to_datetime(response_df["quote_id"], unit="ns")
+
+        # Merge quotations and responses based on date_sent
+        quotation_response_df = quotation_df.merge(
+            response_df, left_on="quotation_date_sent", right_on="quote_id"
+        )
+
+        # Reset the index of the resulting dataframe
+        quotation_response_df.reset_index(drop=True, inplace=True)
+
+        return quotation_response_df
+
+    @staticmethod
+    def calculate_time_diff(
+        df: pd.DataFrame, timestamp1: str, timestamp2: str
+    ) -> pd.DataFrame:
+        """
+        Calculates the time difference between two timestamps and stores the result in a new column 'time_diff'.
+        The time difference is given in seconds. A warning is raised if any time difference is negative.
+
+        Args:
+            df (pd.DataFrame): DataFrame with at least two columns containing timestamps.
+            timestamp1 (str): Name of the first timestamp column.
+            timestamp2 (str): Name of the second timestamp column.
+
+        Returns:
+            pd.DataFrame: DataFrame with an additional 'time_diff' column.
+
+        Raises:
+            TypeError: If df is not a pandas DataFrame or timestamp1 and timestamp2 are not strings.
+            ValueError: If timestamp1 and timestamp2 are not columns in df.
+        """
+        if not isinstance(df, pd.DataFrame):
+            raise TypeError("dataframe must be a pandas DataFrame")
+        if not isinstance(timestamp1, str) or not isinstance(timestamp2, str):
+            raise TypeError("timestamp1 and timestamp2 must be strings")
+
+        try:
+            df["time_diff"] = df.apply(
+                lambda row: 0
+                if row[timestamp1] == row[timestamp2]
+                else (row[timestamp1] - row[timestamp2]) / 1000,
+                axis=1,
+            )
+        except KeyError as e:
+            raise KeyError(f"Could not find column {e.args[0]} in DataFrame")
+
+        if (df["time_diff"] < 0).any():
+            warnings.warn(
+                "There are negative time differences in the data", UserWarning
+            )
+
+        return df
+
+    @staticmethod
+    def calculate_decay_constant(
+        df: pd.DataFrame, column: str, percentile: float = 0.75
+    ) -> float:
+        """
+        Calculates the decay constant based on a specified percentile of a specified column in the given DataFrame.
+
+        Args:
+            df (pd.DataFrame): DataFrame with at least one numerical column.
+            column (str): Name of the column based on which the decay constant is to be calculated.
+            percentile (float, optional): Percentile value to be used in the decay constant calculation.
+                Must be between 0 and 1 (exclusive). Defaults to 0.75.
+
+        Returns:
+            float: Decay constant calculated as log(2) divided by the specified percentile of the specified column.
+
+        Raises:
+            TypeError: If df is not a pandas DataFrame, column is not a string, or percentile is not a float.
+            ValueError: If column is not a column in df or percentile is not between 0 and 1 (exclusive).
+        """
+        if not isinstance(df, pd.DataFrame):
+            raise TypeError(
+                f"Expected 'df' to be a pandas DataFrame, but got {type(df)}"
+            )
+        if not isinstance(column, str):
+            raise TypeError(f"Expected 'column' to be a string, but got {type(column)}")
+        if not isinstance(percentile, float) or not 0 <= percentile < 1:
+            raise ValueError(
+                f"Expected 'percentile' to be a float between 0 and 1 (exclusive), but got {percentile}"
+            )
+
+        try:
+            half_life = df[column].quantile(percentile)
+            decay_constant = np.log(2) / half_life
+        except KeyError as e:
+            raise KeyError(f"Could not find column {e.args[0]} in DataFrame")
+
+        return decay_constant
+
+    @staticmethod
+    def calculate_weight(
+        df: pd.DataFrame, decay_constant: float, base_value: float
+    ) -> pd.DataFrame:
+        """
+        Calculates the weight using the exponential decay function.
+
+        Args:
+            df (pd.DataFrame): DataFrame with at least a numerical column 'time_diff'.
+            decay_constant (float): Decay constant for the calculation.
+            base_value (float): Base value for the weight calculation.
+
+        Returns:
+            pd.DataFrame: Original DataFrame with an additional 'weight' column.
+
+        Raises:
+            TypeError: If df is not a pandas DataFrame, decay_constant is not a float, or base_value is not a float.
+            KeyError: If 'time_diff' column is not in df.
+        """
+        if not isinstance(df, pd.DataFrame):
+            raise TypeError(
+                f"Expected 'df' to be a pandas DataFrame, but got {type(df)}"
+            )
+        if not isinstance(decay_constant, float):
+            raise TypeError(
+                f"Expected 'decay_constant' to be a float, but got {type(decay_constant)}"
+            )
+        if not isinstance(base_value, float):
+            raise TypeError(
+                f"Expected 'base_value' to be a float, but got {type(base_value)}"
+            )
+
+        try:
+            df["weight"] = base_value * np.exp(-decay_constant * df["time_diff"])
+        except KeyError as e:
+            raise KeyError(f"Could not find column {e.args[0]} in DataFrame")
+
+        return df
--- tests/data/test_wrangle_dataframe.py
+++ tests/data/test_wrangle_dataframe.py
@@ -1,3 +1,6 @@
+import itertools
+
+import numpy as np
 import pandas as pd
 import pytest
 from faker import Faker
@@ -100,6 +103,14 @@ class TestDataFrameWrangler:
     Test class for the DataFrameWrangler class.
     """
 
+    @pytest.fixture(autouse=True)
+    def setup_class(self):
+        """
+        Fixture to set up resources before each test method.
+        """
+        self.fake = Faker()
+        self.dataframe_wrangler = DataFrameWrangler()
+
     @pytest.fixture
     def fake_dataframe_messages(self):
         """
@@ -107,17 +118,75 @@ class TestDataFrameWrangler:
         Returns:
             pd.DataFrame: Fake DataFrame with random data.
         """
-        fake = Faker()
+        ids = [self.fake.random_int(min=1, max=1000) for _ in range(100)]
         return pd.DataFrame(
             {
-                "_id": [fake.random_int() for _ in range(10)],
-                "thread_id": [fake.word() for _ in range(10)],
-                "from_recipient_id": [fake.random_int() for _ in range(10)],
-                "quote_id": [fake.random_int() for _ in range(10)],
-                "date_sent": [fake.date_time() for _ in range(10)],
-                "body": [fake.text() for _ in range(10)],
+                "_id": ids,
+                "date_sent": pd.date_range(start="1/1/2018", periods=100),
+                "thread_id": [
+                    self.fake.random_int(min=1, max=1000) for _ in range(100)
+                ],
+                "from_recipient_id": [
+                    self.fake.random_int(min=1, max=1000) for _ in range(100)
+                ],
+                "body": [self.fake.sentence(nb_words=6) for _ in range(100)],
+                "quote_id": ids,  # Using ids as quote_id to mimic reference to another message
             }
         )
+
+    @pytest.fixture
+    def fake_quotation_response_df(self):
+        """
+        Fixture to generate fake DataFrame data for testing quotation-response DataFrame.
+        Returns:
+            pd.DataFrame: Fake DataFrame with random data.
+        """
+        n = 100
+
+        # Generate 'quotation_date_sent' and 'response_date_sent' first
+        quotation_date_sent = [
+            self.fake.random_int(min=1673137959372, max=1673378972279) for _ in range(n)
+        ]
+        response_date_sent = [
+            self.fake.random_int(min=1673137959372, max=1673378972279) for _ in range(n)
+        ]
+
+        # Calculate the time difference in seconds and store in 'time_diff'
+        time_diff = [
+            (start - end) / 1000
+            for start, end in zip(quotation_date_sent, response_date_sent)
+        ]
+
+        data = {
+            "quotation_id": self.fake.random_elements(
+                elements=range(1000), length=n, unique=True
+            ),
+            "quotation_date_sent": quotation_date_sent,
+            "quotation_thread_id": self.fake.random_elements(
+                elements=range(1000), length=n, unique=False
+            ),
+            "quotation_from_recipient_id": self.fake.random_elements(
+                elements=range(1000), length=n, unique=False
+            ),
+            "quotation_body": self.fake.sentences(nb=n, ext_word_list=None),
+            "response_id": self.fake.random_elements(
+                elements=range(1000), length=n, unique=True
+            ),
+            "response_date_sent": response_date_sent,
+            "response_thread_id": self.fake.random_elements(
+                elements=range(1000), length=n, unique=False
+            ),
+            "response_from_recipient_id": self.fake.random_elements(
+                elements=range(1000), length=n, unique=False
+            ),
+            "response_body": self.fake.sentences(nb=n, ext_word_list=None),
+            "quote_id": self.fake.random_elements(
+                elements=range(1000), length=n, unique=False
+            ),
+            "time_diff": time_diff,
+        }
+
+        return pd.DataFrame(data)
 
     @pytest.fixture
     def fake_dataframe_reactions(self):
@@ -126,27 +195,18 @@ class TestDataFrameWrangler:
         Returns:
             pd.DataFrame: Fake DataFrame with random data.
         """
-        fake = Faker()
         return pd.DataFrame(
             {
-                "_id": [fake.random_int() for _ in range(10)],
-                "message_id": [fake.random_int() for _ in range(10)],
-                "author_id": [fake.random_int() for _ in range(10)],
+                "_id": [self.fake.random_int() for _ in range(10)],
+                "message_id": [self.fake.random_int() for _ in range(10)],
+                "author_id": [self.fake.random_int() for _ in range(10)],
                 "emoji": [
-                    fake.random_element(elements=("😍", "🐍", "❤️", "👍", "🙌"))
+                    self.fake.random_element(elements=("😍", "🐍", "❤️", "👍", "🙌"))
                     for _ in range(10)
                 ],
-                "date_sent": [fake.date_time() for _ in range(10)],
+                "date_sent": [self.fake.date_time() for _ in range(10)],
             }
         )
-
-    @pytest.fixture(autouse=True)
-    def setup_class(self):
-        """
-        Fixture to set up resources before each test method.
-        """
-        self.fake = Faker()
-        self.dataframe_wrangler = DataFrameWrangler()
 
     @pytest.mark.parametrize("iteration", range(10))
     def test_filter_and_rename_messages_df(self, iteration, fake_dataframe_messages):
@@ -241,3 +301,187 @@ class TestDataFrameWrangler:
         # Passing None as input
         with pytest.raises(ValueError, match="dataframe must be a pandas DataFrame"):
             self.dataframe_wrangler.filter_and_rename_reactions_df(None)
+
+    @pytest.mark.parametrize("iteration", range(10))
+    def test_create_quotation_response_df_no_quotation(
+        self, iteration, fake_dataframe_messages
+    ):
+        """
+        Test to check if create_quotation_response_df() correctly handles the case where a message has no quotation.
+        """
+        # Modify the fake DataFrame so that the first entry has no quotation
+        fake_dataframe_messages.loc[0, "quote_id"] = None
+
+        quotation_response_df = DataFrameWrangler.create_quotation_response_df(
+            fake_dataframe_messages
+        )
+
+        # If there are no quotations, the DataFrame should be empty
+        assert quotation_response_df.empty
+
+    @pytest.mark.parametrize("iteration", range(10))
+    def test_create_quotation_response_df_invalid_input(self, iteration):
+        """
+        Test to check if create_quotation_response_df() raises an error when given invalid input.
+        """
+        # Try calling create_quotation_response_df() with non-DataFrame inputs
+        with pytest.raises(TypeError, match="dataframe must be a pandas DataFrame"):
+            DataFrameWrangler.create_quotation_response_df("invalid_input")
+
+    @pytest.mark.parametrize("iteration", range(10))
+    def test_calculate_time_diff(self, iteration, fake_quotation_response_df):
+        """
+        Test to verify that the function `calculate_time_diff` correctly calculates the time difference
+        between quotation and response date_sent and adds it as a new column to the DataFrame.
+        """
+        # Use the calculate_time_diff() function to calculate the time difference
+        time_diff_df = self.dataframe_wrangler.calculate_time_diff(
+            fake_quotation_response_df, "quotation_date_sent", "response_date_sent"
+        )
+
+        # Check that the output is a DataFrame
+        assert isinstance(time_diff_df, pd.DataFrame)
+
+        # Check that the 'time_diff' column exists in the output DataFrame
+        assert "time_diff" in time_diff_df.columns
+
+        # Check that a warning is raised when time_diff is negative
+        with pytest.warns(
+            UserWarning, match="There are negative time differences in the data"
+        ):
+            self.dataframe_wrangler.calculate_time_diff(
+                fake_quotation_response_df, "quotation_date_sent", "response_date_sent"
+            )
+
+    @pytest.mark.parametrize("iteration", range(10))
+    def test_calculate_time_diff_invalid_input(
+        self, iteration, fake_quotation_response_df
+    ):
+        """
+        Test to check if calculate_time_diff() raises an error when given invalid input.
+        """
+        # Try calling calculate_time_diff() with non-DataFrame inputs
+        with pytest.raises(TypeError, match="dataframe must be a pandas DataFrame"):
+            self.dataframe_wrangler.calculate_time_diff(
+                "invalid_data", "quotation_date_sent", "response_date_sent"
+            )
+
+        # Try calling calculate_time_diff() with DataFrame but non-existing column names
+        with pytest.raises(KeyError, match="Could not find column"):
+            self.dataframe_wrangler.calculate_time_diff(
+                fake_quotation_response_df,
+                "non_existent_start_time",
+                "non_existent_end_time",
+            )
+
+        # Check if the function correctly calculates time difference even when timestamps are equal
+        fake_quotation_response_df_equal_timestamps = fake_quotation_response_df.copy()
+        fake_quotation_response_df_equal_timestamps[
+            "quotation_date_sent"
+        ] = fake_quotation_response_df_equal_timestamps["response_date_sent"]
+
+        result_df = self.dataframe_wrangler.calculate_time_diff(
+            fake_quotation_response_df_equal_timestamps,
+            "quotation_date_sent",
+            "response_date_sent",
+        )
+
+        assert (
+            result_df["time_diff"] == 0
+        ).all(), "Time difference not correctly calculated for equal timestamps."
+
+    @pytest.mark.parametrize(
+        "iteration, percentile", itertools.product(range(10), [0.5, 0.75, 0.99])
+    )
+    def test_calculate_decay_constant(
+        self, iteration, percentile, fake_quotation_response_df
+    ):
+        """
+        Test to verify that the function `calculate_decay_constant` correctly calculates the decay constant based
+        on the specified percentile of a specified column in the given DataFrame.
+        """
+        # Use the calculate_decay_constant() function to calculate the decay constant
+        decay_constant = self.dataframe_wrangler.calculate_decay_constant(
+            fake_quotation_response_df, "time_diff", percentile
+        )
+
+        # Check that the output is a float
+        assert isinstance(decay_constant, float)
+
+        # Try calling calculate_decay_constant() with DataFrame but non-existing column names
+        with pytest.raises(KeyError, match="Could not find column"):
+            self.dataframe_wrangler.calculate_decay_constant(
+                fake_quotation_response_df, "non_existent_column", percentile
+            )
+
+        # Try calling calculate_decay_constant() with invalid percentile
+        with pytest.raises(
+            ValueError, match="Expected 'percentile' to be a float between 0 and 1"
+        ):
+            self.dataframe_wrangler.calculate_decay_constant(
+                fake_quotation_response_df, "time_diff", -1
+            )
+
+        # Check the function with default percentile
+        default_decay_constant = self.dataframe_wrangler.calculate_decay_constant(
+            fake_quotation_response_df, "time_diff"
+        )
+        default_half_life = fake_quotation_response_df["time_diff"].quantile(0.75)
+        assert abs(default_decay_constant - np.log(2) / default_half_life) < 1e-6
+
+    @pytest.mark.parametrize("iteration", range(10))
+    def test_calculate_weight(self, iteration, fake_quotation_response_df):
+        """
+        Test to verify that the function `calculate_weight` correctly calculates the weight using the exponential
+        decay function and adds it as a new column to the DataFrame.
+        """
+        # Arbitrarily choose a decay constant for this test
+        decay_constant = self.fake.random.uniform(0, 2)
+
+        # Generate a random float for the base value using self.fake
+        base_value = self.fake.random.uniform(0, 2)
+
+        # Use the calculate_weight() function to add 'weight' column to the DataFrame
+        df_with_weight = self.dataframe_wrangler.calculate_weight(
+            fake_quotation_response_df, decay_constant, base_value
+        )
+
+        # Check that the 'weight' column has been added
+        assert "weight" in df_with_weight.columns
+
+        # Check that the 'weight' column values have been correctly calculated
+        expected_weights = base_value * np.exp(
+            -decay_constant * fake_quotation_response_df["time_diff"]
+        )
+        pd.testing.assert_series_equal(
+            df_with_weight["weight"], expected_weights, check_names=False
+        )
+
+        # Try calling calculate_weight() with non-DataFrame, non-float decay_constant, and non-float base_value
+        with pytest.raises(
+            TypeError, match="Expected 'df' to be a pandas DataFrame, but got"
+        ):
+            self.dataframe_wrangler.calculate_weight(
+                "not_a_dataframe", decay_constant, base_value
+            )
+        with pytest.raises(
+            TypeError, match="Expected 'decay_constant' to be a float, but got"
+        ):
+            self.dataframe_wrangler.calculate_weight(
+                fake_quotation_response_df, "not_a_float", base_value
+            )
+        with pytest.raises(
+            TypeError, match="Expected 'base_value' to be a float, but got"
+        ):
+            self.dataframe_wrangler.calculate_weight(
+                fake_quotation_response_df, decay_constant, "not_a_float"
+            )
+
+        # Try calling calculate_weight() with a DataFrame without 'time_diff' column
+        df_without_time_diff = fake_quotation_response_df.drop(columns=["time_diff"])
+        with pytest.raises(
+            KeyError, match="Could not find column time_diff in DataFrame"
+        ):
+            self.dataframe_wrangler.calculate_weight(
+                df_without_time_diff, decay_constant, base_value
+            )
