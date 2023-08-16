527857233983fc18b5baf572a9ba2788417756ee
Raymond Pasek <rcpasek@gmail.com>
2023-08-15 21:18:05 -0400

    more scratch work

--- src/data/sna_exploration.py
+++ src/data/sna_exploration.py
@@ -5,25 +5,26 @@ import pandas as pd
 from src.data.csv_importer import CSVImporter
 from src.data.data_wrangling import (DateTimeConverter, MessageDataWrangler,
                                      QuotationResponseDataWrangler,
-                                     ReactionDataWrangler)
+                                     EmojiDataWrangler)
+from src.data.sna_preparation import SnaDataWrangler
 
-from src.data.recipient_mapper import RecipientMapper
 from src.data.time_calculations import TimeCalculations
 
 
 # Import message.csv and reaction.csv
 message_df = CSVImporter.import_csv("message")
-reaction_df = CSVImporter.import_csv("reaction")
+emoji_df = CSVImporter.import_csv("reaction")
 
 # Filter and rename columns in message_df
 message_slim_df = MessageDataWrangler.filter_and_rename_messages_df(message_df, thread_id=2)
+
+
+
 
 # Concatenate comment threads to pair comments and responses, and sets the number of rows to look forward for each
 # individual comment to the total number of unique comment_from_recipient_ids.
 group_n = message_slim_df["comment_from_recipient_id"].nunique()
 comments_responses_df = MessageDataWrangler.concatenate_comment_threads(message_slim_df, group_n)
-
-
 
 # Quantify the time difference between comments and responses
 response_time_df = TimeCalculations.calculate_time_diff(comments_responses_df, "response_date_sent", "comment_date_sent")
@@ -38,26 +39,34 @@ response_weight_df = TimeCalculations.calculate_weight(response_time_df, respons
 response_weight_df = DateTimeConverter.convert_unix_to_datetime(response_weight_df, "comment_date_sent")
 response_weight_df = DateTimeConverter.convert_unix_to_datetime(response_weight_df, "response_date_sent")
 
+# Subset the response_weight_df and add an interaction category for each record
+response_cols = ["comment_from_recipient_id", "comment_date_sent_datetime", "response_from_recipient_id", "response_date_sent_datetime", "weight"]
+response_weight_slim_df = SnaDataWrangler.create_reacted_dataframe(response_weight_df, response_cols, "response")
 
 
-# Filter and rename columns in reaction_df
-reaction_slim_df = ReactionDataWrangler.filter_and_rename_reactions_df(reaction_df)
 
-# Merge messages to reactions
-comments_reactions_df = ReactionDataWrangler.merge_message_with_reaction(message_slim_df, reaction_slim_df)
+# Filter and rename columns in emoji_df
+emoji_slim_df = EmojiDataWrangler.filter_and_rename_emojis_df(emoji_df)
 
-# Quantify the time difference between comments and reactions
-reactions_time_df = TimeCalculations.calculate_time_diff(comments_reactions_df, "reaction_date_sent", "comment_date_sent")
+# Merge messages to emojis
+comments_emojis_df = EmojiDataWrangler.merge_message_with_emoji(message_slim_df, emoji_slim_df)
+
+# Quantify the time difference between comments and emojis
+emojis_time_df = TimeCalculations.calculate_time_diff(comments_emojis_df, "emoji_date_sent", "comment_date_sent")
 
 # Calculate the decay constant based on the 75 percentile of the time_diff column
-reaction_decay_constant = TimeCalculations.calculate_decay_constant(reactions_time_df, "time_diff")
+emoji_decay_constant = TimeCalculations.calculate_decay_constant(emojis_time_df, "time_diff")
 
 # Using a base value of 1.5, derive the weight of each comment-response interaction
-reaction_weight_df = TimeCalculations.calculate_weight(reactions_time_df, reaction_decay_constant, 1.5)
+emoji_weight_df = TimeCalculations.calculate_weight(emojis_time_df, emoji_decay_constant, 1.5)
 
 # Create more readable datetime columns
-reaction_weight_df = DateTimeConverter.convert_unix_to_datetime(reaction_weight_df, "comment_date_sent")
-reaction_weight_df = DateTimeConverter.convert_unix_to_datetime(reaction_weight_df, "reaction_date_sent")
+emoji_weight_df = DateTimeConverter.convert_unix_to_datetime(emoji_weight_df, "comment_date_sent")
+emoji_weight_df = DateTimeConverter.convert_unix_to_datetime(emoji_weight_df, "emoji_date_sent")
+
+# Subset the emoji_weight_df and add an interaction category for each record
+emoji_cols = ["comment_from_recipient_id", "comment_date_sent_datetime", "emoji_author_id", "emoji_date_sent_datetime", "weight"]
+emoji_weight_slim_df = SnaDataWrangler.create_reacted_dataframe(emoji_weight_df, emoji_cols, "emoji")
 
 
 
@@ -76,3 +85,7 @@ quotation_weight_df = TimeCalculations.calculate_weight(quotation_time_df, quota
 # Create more readable datetime columns
 quotation_weight_df = DateTimeConverter.convert_unix_to_datetime(quotation_weight_df, "quotation_date_sent")
 quotation_weight_df = DateTimeConverter.convert_unix_to_datetime(quotation_weight_df, "response_date_sent")
+
+# Subset the response_weight_df and add an interaction category for each record
+quotation_cols = ["quotation_from_recipient_id", "quotation_date_sent_datetime", "response_from_recipient_id", "response_date_sent_datetime", "weight"]
+quotation_weight_slim_df = SnaDataWrangler.create_reacted_dataframe(quotation_weight_df, quotation_cols, "quotation")
