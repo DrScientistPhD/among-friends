52f20c162c951a7310f9a4fb7b05b5d03d3eb4f9
Raymond Pasek <rcpasek@gmail.com>
2023-09-15 14:58:10 -0400

    add explanatory text to sna app page

--- src/visualization/social_network_page.py
+++ src/visualization/social_network_page.py
@@ -1,6 +1,6 @@
 import panel as pn
 
-pn.extension(loading_spinner='dots')
+pn.extension(loading_spinner="dots")
 
 
 class SocialNetworkPage:
@@ -34,7 +34,9 @@ class SocialNetworkPage:
             # Set up watchers to update views when widget values change
             self.setup_watchers()
         except Exception as e:
-            raise Exception(f"Error encountered while initializing SocialNetworkPage: {e}")
+            raise Exception(
+                f"Error encountered while initializing SocialNetworkPage: {e}"
+            )
 
     def initialize_graph_pane(self):
         """
@@ -60,18 +62,18 @@ class SocialNetworkPage:
 
     def initialize_closeness_table(self, min_date, max_date):
         """
-         Initializes and returns the closeness table for a given date range.
+        Initializes and returns the closeness table for a given date range.
 
-         Args:
-             min_date: Minimum date for the date range.
-             max_date: Maximum date for the date range.
+        Args:
+            min_date: Minimum date for the date range.
+            max_date: Maximum date for the date range.
 
-         Returns:
-             pn.widgets.Tabulator: A table widget displaying closeness rankings.
+        Returns:
+            pn.widgets.Tabulator: A table widget displaying closeness rankings.
 
-         Raises:
-             Exception: If there's an error while generating the closeness table.
-         """
+        Raises:
+            Exception: If there's an error while generating the closeness table.
+        """
         try:
             common_table_options = dict(
                 page_size=20,
@@ -90,7 +92,9 @@ class SocialNetworkPage:
                 value=closeness_table_data, editors=editors, **common_table_options
             )
         except Exception as e:
-            raise Exception(f"Error encountered while initializing closeness table: {e}")
+            raise Exception(
+                f"Error encountered while initializing closeness table: {e}"
+            )
 
     def initialize_sidebar(self):
         """
@@ -114,12 +118,38 @@ class SocialNetworkPage:
         except Exception as e:
             raise Exception(f"Error encountered while initializing sidebar: {e}")
 
+    def initialize_explanatory_text(self):
+        """
+        Initializes and returns the specified text.
+
+        Returns:
+            pn.pane.Markdown: A Markdown pane containing the specified text.
+        """
+        explanatory_text = """\
+        ### Understanding the Network Analysis Page
+        <div style="font-size: 16px;">
+        
+        Use the *Group Chat Date Range* slider to set the time period, and the *Group Chat Participant* dropdown menu to
+         select the participant of interest. 
+         
+         <br> **Influence Ranking** is a rank measure of the selected 
+         participant's influence in the group chat for that time period, ultimately derived from the eigenvector 
+         centrality score of text responses, quotation responses, and emoji reactions to other text messages. 
+         Interactions are weighted according to the time difference between the source text message and the response. 
+         
+         <br>**Outward Response Rankings** is a measure of how active each participant is responding to other 
+         participants and how strong those responses are based on frequency and timing. A better ranking (with 1 being 
+         the best) indicates the selected participant is responding more strongly to the ranked participant.  
+        </div>
+        """
+        return pn.pane.Markdown(explanatory_text)
+
     def initialize_content(self):
         """
         Initializes and returns the main content layout.
 
         Returns:
-            pn.Row: A row layout containing the sidebar, graph, and closeness table.
+            pn.Row: A row layout containing the sidebar, graph, closeness table, and explanatory text.
 
         Raises:
             Exception: If there's an error while generating the content layout.
@@ -129,14 +159,29 @@ class SocialNetworkPage:
                 "### Outward Response Rankings"
             )
             outward_response_table_layout = pn.Column(
-                outward_response_table_title, self.closeness_table, sizing_mode="stretch_width"
+                outward_response_table_title,
+                self.closeness_table,
+                sizing_mode="stretch_width",
             )
-            return pn.Row(
+
+            # Get the explanatory text
+            explanatory_text = self.initialize_explanatory_text()
+
+            # Combine the content, explanatory text, and layout
+            content = pn.Row(
                 self.sidebar,
                 self.graph_pane,
                 outward_response_table_layout,
+            )
+
+            # Add the explanatory text at the end
+            layout = pn.Column(
+                content,
+                explanatory_text,
                 sizing_mode="stretch_width",
             )
+
+            return layout
         except Exception as e:
             raise Exception(f"Error encountered initializing content: {e}")
 
