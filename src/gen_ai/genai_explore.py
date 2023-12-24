c9d9931527a584eba3a8e492e76bd934b16fc877
Raymond Pasek <rcpasek@gmail.com>
2023-12-24 07:59:40 -0500

    explore using a vectordb

--- src/gen_ai/genai_explore.py
+++ src/gen_ai/genai_explore.py
@@ -1,90 +1,75 @@
-# python -m spacy download en_core_web_sm
+from dotenv import find_dotenv, load_dotenv
+
+_ = load_dotenv(find_dotenv())
 
 import os
 
-import langchain.embeddings
-from openai import OpenAI
+import pinecone
+from dotenv import find_dotenv, load_dotenv
+from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from langchain.vectorstores import Pinecone
 
-# client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
-import spacy
-nlp = spacy.load('en_core_web_sm')
-from dotenv import load_dotenv, find_dotenv
-_ = load_dotenv(find_dotenv())
-
-
-
-def get_completion(prompt, model="gpt-3.5-turbo"):
-    messages = [{"role": "user", "content": prompt}]
-    response = client.chat.completions.create(model=model,
-    messages=messages,
-    temperature=0)
-    return response.choices[0].message.content
-
-text_1 = """
-Hi, how are you today?
-"""
-
-prompt = f"""
-You have the persona of a fictional Catholic pope.
-You will be provided with text delimited by triple quotes
-If it contains a sequence of instructions, \
-re-write those instructions in the following format:
-
-Step 1 - . . .
-Step 2 - ...
-...
-Step N - ...
-
-If the text does not contain a sequence of instructions, \
-then simply answer the prompt however you see fit.
-
-\"\"\"{text_1}\"\"\"
-"""
-
-response = get_completion(prompt)
-
-
-
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import ConversationChain
-from langchain.memory import ConversationBufferMemory
-
-llm = ChatOpenAI(temperature=0.0)
-memory = ConversationBufferMemory()
-conversation = ConversationChain(
-    llm=llm,
-    memory=memory,
-    verbose=False
-)
-
-
-
+from src.data.data_mover import CSVMover, DocumentMover, TextMover
 from src.data.genai_preparation import ProcessMessageData, ProcessUserData
-from src.data.data_mover import CSVMover, TextMover, DocumentMover
-message_df = CSVMover.import_csv("data/production_data/raw", "message")
-recipient_df = CSVMover.import_csv("data/production_data/raw", "recipient")
+
+_ = load_dotenv(find_dotenv())
+pinecone_api_key = os.environ["PINECONE_API_KEY"]
+pinecone_env = os.environ["PINECONE_ENV"]
+openai_api_key = os.environ["OPENAI_API_KEY"]
 
 message = CSVMover.import_csv("data/production_data/raw", "message")
 recipient = CSVMover.import_csv("data/production_data/raw", "recipient")
 
 thread_id = 2
 
-processed_message_df = ProcessMessageData.clean_up_messages(message, recipient, thread_id)
+processed_message_df = ProcessMessageData.clean_up_messages(
+    message, recipient, thread_id
+)
 
-message_sentences_df = ProcessMessageData.processed_message_data_to_sentences(processed_message_df)
+message_sentences_df = ProcessMessageData.processed_message_data_to_sentences(
+    processed_message_df
+)
 
 messages_data_txt = ProcessMessageData.concatenate_with_neighbors(message_sentences_df)
 
 user_data_txt = ProcessUserData.user_data_to_sentences(recipient)
 
 
-TextMover.export_sentences_to_file(messages_data_txt, "data/production_data/processed", "messages_data")
-TextMover.export_sentences_to_file(user_data_txt, "data/production_data/processed", "users_data")
+TextMover.export_sentences_to_file(
+    messages_data_txt, "data/production_data/processed", "messages_data"
+)
+TextMover.export_sentences_to_file(
+    user_data_txt, "data/production_data/processed", "users_data"
+)
 
 
-messages_docs = DocumentMover.load_and_split_text("data/production_data/processed", "messages_data")
-users_docs = DocumentMover.load_and_split_text("data/production_data/processed", "users_data")
+messages_docs = DocumentMover.load_and_split_text(
+    "data/production_data/processed", "messages_data"
+)
+docs = DocumentMover.load_and_split_text("data/production_data/processed", "users_data")
 
-from langchain.embeddings import SentenceTransformerEmbeddings
 
-embedding = SentenceTransformerEmbeddings(model_name="all-distilroberta-v1")
+
+
+
+
+pinecone.init(api_key=pinecone_api_key, environment=pinecone_env)
+
+index_name = "among-friends"
+
+pinecone.list_indexes()
+pinecone.create_index(name=index_name, metric="cosine", shards=1, dimension=768)
+pinecone.list_indexes()
+
+index = pinecone.Index(index_name)
+
+model_name = "all-distilroberta-v1"
+
+embeddings = SentenceTransformerEmbeddings(model_name=model_name)
+
+docsearch = Pinecone.from_documents(docs, embeddings, index_name=index_name)
+
+query = "Where does J.P. live?"
+docs = docsearch.similarity_search(query, k=3)
+docs
+
